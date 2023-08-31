bd28fe6eaaafe4c3ff6fe0b3b9d7d9410ce7d88d
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2023-08-31 08:27:25 +0530

    Add files via upload

---
+++ string length.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int c,i;
+	char a[100];
+	printf("enter the string: ");
+	scanf("%s",&a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		c=i;
+	}
+	printf("%d",c);
+}
